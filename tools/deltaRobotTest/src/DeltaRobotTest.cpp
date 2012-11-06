aca81a804fbee7cc85cc75508eab17c0adf5f43e
pndewit <dodoiva@gmail.com>
2012-11-06 16:24:09 +0100

    z value changed

--- tools/deltaRobotTest/src/DeltaRobotTest.cpp
+++ tools/deltaRobotTest/src/DeltaRobotTest.cpp
@@ -123,7 +123,8 @@ int main(int argc, char **argv){
 	std:: cin >> keyPress;   
 	moveToPointService.request.motion.x = 0;
 	moveToPointService.request.motion.y = 0;
-	moveToPointService.request.motion.z = -85;
+	moveToPointService.request.motion.z = -25;
+	//moveToPointService.request.motion.z = -85;
 	moveToPointService.request.motion.speed = speed;
 	moveToPointClient.call(moveToPointService);
 
