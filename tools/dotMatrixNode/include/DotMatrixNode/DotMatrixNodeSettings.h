76086544a28e91c986709fdc6b3f36dfc47be181
Koen Braham <koenbraham@gmail.com>
2012-11-07 09:58:14 +0100

    Added DotMatrix node to the libraries include thingy. Updated the drawingfield size.

--- tools/dotMatrixNode/include/DotMatrixNode/DotMatrixNodeSettings.h
+++ tools/dotMatrixNode/include/DotMatrixNode/DotMatrixNodeSettings.h
@@ -1,24 +1,24 @@
 namespace DotMatrixNodeSettings {
 
-	static const unsigned int DRAW_FIELD_START_X = 0;
-	static const unsigned int DRAW_FIELD_START_Y = 0;
-
 	/**
 	 * @var static const unsigned int DRAW_FIELD_HEIGHT
 	 * Height of the field that is drawable by the deltarobot.
 	 */
-	static const unsigned int DRAW_FIELD_HEIGHT = 100;
+	static const unsigned int DRAW_FIELD_HEIGHT = 175;
 
 	/**
 	 * @var static const unsigned int DRAW_FIELD_WIDTH
 	 * Width of the field that is drawable by the deltarobot.
 	 */
-	static const unsigned int DRAW_FIELD_WIDTH = 100;
+	static const unsigned int DRAW_FIELD_WIDTH = 110;
 
 	/**
-	 * @var static const unsigned double DRAW_FIELD_DOTS_PER_MM
+	 * @var static const double DRAW_FIELD_DOTS_PER_MM
 	 * Number of dots per millimeter of the drawing field
 	 */
 	static const double DRAW_FIELD_DOTS_PER_MM = 2;
+
+	static const double DRAW_FIELD_Z_LOW = 300;//TODO: ?
+	static const double DRAW_FIELD_Z_HIGH = 280;//TODO: ?
 
 }
